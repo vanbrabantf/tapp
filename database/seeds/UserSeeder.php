bfdf74c1625448e98ae97ecd45cc666f18464806
frederick Vanbrabant <frederick@brodyandhooper.com>
2020-07-09 14:59:28 +0200

    Moved the User class to the models folder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 10)->create();
+    }
+}

f10f7256d6c6078fcd0654688312fcfe19e14c1e
frederick Vanbrabant <frederick@brodyandhooper.com>
2020-07-09 16:51:21 +0200

    Added the controllers to handle the routes

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Users\UserRepository;
+use Illuminate\View\View;
+
+final class AdminController extends Controller
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->middleware('auth');
+        $this->userRepository = $userRepository;
+    }
+
+    public function index(): View
+    {
+        $users = $this->userRepository->getAllUsers();
+
+        return view('admin', ['users' => $users]);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <nav>
+                            <a class="btn btn-link" href="{{ route('admin') }}">
+                                {{ __('Admin Page') }}
+                            </a>
+                            <a class="btn btn-link" href="{{ route('unapproved') }}">
+                                {{ __('Unapproved page') }}
+                            </a>
+                            <a class="btn btn-link" href="{{ route('approved') }}">
+                                {{ __('Approved page') }}
+                            </a>
+                        </nav>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
